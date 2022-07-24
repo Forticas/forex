d65cb5a334bc96f719ba9f1029b5165b7d15ea82
Houssem TAYECH <houssem@forticas.com>
2022-07-24 23:12:18 +0200

    add test controller

--- src/Controller/Admin/ForecastLinkCrudController.php
+++ src/Controller/Admin/ForecastLinkCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\ForecastLink;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
@@ -19,7 +20,7 @@ class ForecastLinkCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-
+            IdField::new('id'),
             TextField::new('title'),
             UrlField::new('url'),
             DateTimeField::new('fetchedAt'),
